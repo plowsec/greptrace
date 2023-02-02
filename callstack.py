349f702a5e3dd45abbd8d4c55941efd3d894db5c
plowsec <united.marshmallow@gmail.com>
2023-02-02 11:03:13 +0100

    Upload source code

---
+++ callstack.py
@@ -0,0 +1,267 @@
+from os import listdir
+from os.path import isfile, getmtime, join
+import os
+import sys
+import logging
+from dataclasses import dataclass
+from copy import deepcopy
+logging.basicConfig(level=logging.DEBUG)
+import re
+
+@dataclass
+class Chain:
+
+    stack = []
+    usages = {} # dict, keys are levels in the call stack tree, values are list of files containing the pattern
+    current_pattern = ""
+    depth= 0
+    reachable = False
+    source_params = set()
+    source_functions = set()
+
+
+    def __str__(self):
+
+        res = ""
+        res += self.stack[0] + "\n"
+        for call in self.stack[1:]:
+            res += "\t-> " + call + "\n"
+        return res
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+
+        return self.stack == other.stack
+
+
+def get_translation_units(root_dir, whitelist=[]):
+
+    tunits= []
+
+    for (dirpath, dirs, files) in os.walk(root_dir):
+
+        for filename in files:
+
+            filename = os.path.join(dirpath, filename)
+
+            if len(whitelist) > 0 and any(x in os.path.abspath(filename) for x in whitelist):
+
+                if os.path.splitext(filename)[1] in [".jsp", ".java"]:
+                    tunits += [filename]
+
+            elif len(whitelist) == 0:
+                if os.path.splitext(filename)[1] in [".jsp", ".java"]:
+                    tunits += [filename]
+
+        for folder in dirs:
+
+            tunits += get_translation_units(folder, whitelist)
+
+    return tunits
+
+def find_file_containing_string(pattern, files, blacklist=[], concatenation_only=False):
+
+    found_files = []
+
+    for file in files:
+
+        if file in blacklist:
+            continue
+
+        with open(file) as fl:
+            found = next((l for l in fl if pattern in l), None) #find first occurrence in file
+        if found:
+
+            if concatenation_only:
+                with open(file) as fl:
+                    for line in fl:
+
+                        if pattern in line:
+
+                            if re.search(r"\".*\" ?\+", line, re.MULTILINE) is not None:
+                                found_files += [file]
+                                break
+            else:
+
+                found_files += [file]
+
+    return found_files
+
+
+def extract_class_name(file):
+
+    class_name = ""
+    if os.path.splitext(file)[1] == ".jsp":
+        return os.path.basename(file)
+    with open(file) as fl:
+        for line in fl:
+
+            if any(x in line for x in ["class ", "interface ", "enum "]):
+                class_name = re.split("class |interface |enum", line)[1].split()[0]
+                if class_name in os.path.basename(file):
+                    return class_name
+
+
+    if class_name == "":
+        raise Exception("Cannot find class name")
+    return class_name
+
+def is_source(file, pattern):
+
+    pattern_param = "getParameter("
+    pattern_servlet = "HttpServletRequest"
+
+    regex = (r"(?:(?:public|private|protected|static|final|native|synchronized|abstract|transient)+\s+)+[$_\w<>\[\]\s]*\s+([\$_\w]+)\([^\)]*\)?\s*\{?[^\}]*\}?\n")
+    is_source_found = False
+    with open(file) as fl:
+        found = next((l for l in fl if pattern_param in l), None) #find first occurrence in file
+        if found:
+            # read the whole file again and locate the usage
+
+            current_function = ""
+            found_get_param = False
+            found_pattern = False
+            for line in fl:
+
+                if (pattern_param in line or pattern_servlet in line) and current_function != "":
+                    # found getparam
+                    found_get_param = True
+                if pattern in line and current_function != "":
+                    found_pattern = True
+
+                if found_pattern and found_get_param:
+                    #logging.info(f"Pattern {pattern} was found in controller {current_function}")
+                    is_source_found = True
+                if re.search(regex, line) is not None:
+                    matches = re.finditer(regex, line, re.MULTILINE)
+
+                    found_get_param = False
+                    found_pattern = False
+
+                    match = next(matches)
+                    current_function = match.group(1)
+                    #logging.info(f"Current function = {current_function}")
+
+    return is_source_found
+
+def find_usages(chain, all_files, blacklist=[], depth=3, max_patterns=5):
+
+
+    all_usages = find_file_containing_string(chain.current_pattern, all_files, blacklist=chain.stack)
+    #logging.info(f"Usage detected in : {all_usages}")
+
+    chain.depth += 1
+
+
+    chain.usages[chain.depth] = all_usages
+    chains = [chain]
+
+    if chain.depth >= depth:
+        return chains
+
+    for file in all_usages[:max_patterns]:
+
+        logging.info(f"Found '{chain.current_pattern}' pattern in {file}")
+        class_name = extract_class_name(file)
+
+        new_chain = deepcopy(chain)
+        new_chain.current_pattern = class_name
+        new_chain.stack += [file]
+
+        if not is_source(file, chain.current_pattern):
+            chains += find_usages(new_chain, all_files, blacklist + [file], depth=depth, max_patterns=max_patterns)
+        else:
+            new_chain.reachable = True
+            chains += [new_chain]
+
+    return chains
+
+def extract_attack_surface(chain):
+
+    current_params = set()
+    params = set()
+    file = chain.stack[-1]
+    is_source_found = False
+    functions = set()
+    pattern_param = "getParameter("
+    pattern_servlet = "HttpServletRequest"
+
+    regex = (r"(?:(?:public|private|protected|static|final|native|synchronized|abstract|transient)+\s+)+[$_\w<>\[\]\s]*\s+([\$_\w]+)\([^\)]*\)?\s*\{?[^\}]*\}?\n")
+    pattern = os.path.basename(chain.stack[-2]).split(".java")[0]
+
+    with open(file) as fl:
+
+        current_function = ""
+        found_get_param = False
+        found_pattern = False
+        for line in fl:
+
+            if "getParameter(\"" in line:
+                param_name = line.split("getParameter(\"")[1].split("\")")[0]
+                current_params.add(param_name)
+
+            if (pattern_param in line or pattern_servlet in line) and current_function != "":
+                # found getparam
+                found_get_param = True
+            if pattern in line and current_function != "":
+                found_pattern = True
+
+            if found_pattern and found_get_param:
+                is_source_found = True
+                functions.add(current_function)
+            if re.search(regex, line) is not None:
+
+                if is_source_found:
+                    params = set(list(current_params) + list(params))
+                    current_params.clear()
+                    is_source_found = False
+                matches = re.finditer(regex, line, re.MULTILINE)
+
+                found_get_param = False
+                found_pattern = False
+                current_params.clear()
+
+                match = next(matches)
+                current_function = match.group(1)
+
+    return params, functions
+
+
+def build_callstack(folder_path, pattern="getWhere", restrict_to_file="", concatenation_only=False, depth=3, max_patterns=5, max_results=-1):
+
+    all_files = get_translation_units(os.path.abspath(folder_path))
+    files = find_file_containing_string(pattern, all_files, concatenation_only=concatenation_only)
+    logging.info("Warning: debug on, only EPODirSortServices is analyzed")
+
+    chains = []
+    for file in files[:max_results]:
+
+        # for testing only
+        if restrict_to_file != "" and os.path.basename(file) != restrict_to_file:
+            continue
+
+        logging.info(f"Found pattern in {file}")
+        class_name = extract_class_name(file)
+        logging.info(f"Class name = {class_name}")
+        chain = Chain()
+        chain.stack = [file]
+        chain.current_pattern = class_name
+        chains += find_usages(chain, all_files, blacklist=chain.stack, depth=depth, max_patterns=max_patterns)
+
+    uniq_chains = [v1 for i, v1 in enumerate(chains) if not any(v1 == v2 for v2 in chains[:i])]
+
+    for chain in uniq_chains:
+        if chain.reachable:
+            logging.info(chain)
+            params, functions = extract_attack_surface(chain)
+            chain.source_params = params
+            chain.source_functions = functions
+            logging.info(f"You should fuzz these functions ({functions}), which have at least the following params: {params}")
+
+    return uniq_chains
+
+if __name__ == "__main__":
+
+    build_callstack()
---
+++ main.py
@@ -0,0 +1,60 @@
+import callstack
+import j2ee_attack_surface
+
+import argparse
+import sys
+import logging
+import os
+
+logging.basicConfig(level=logging.DEBUG)
+
+LANGUAGE_EXTENSIONS = [".java", ".jsp"]
+
+def pwn(webxml_path, source_dir, pattern="getWhere", restrict_to_file="EPODirSortServices.java", concatenation_only=False, depth=3, max_patterns=5, extensive=False, max_results=10):
+
+    endpoints_standard = j2ee_attack_surface.parse(webxml_path)
+    endpoints_beans = j2ee_attack_surface.parse_beans(webxml_path)
+    chains = callstack.build_callstack(source_dir, pattern, restrict_to_file, concatenation_only, depth, max_patterns, max_results)
+
+    print(endpoints_standard)
+    print(endpoints_beans)
+    print(chains)
+
+
+    for chain in chains:
+
+        class_name = os.path.splitext(os.path.basename(chain.stack[-1]))[0]
+        for method in chain.source_functions:
+
+            for bean in endpoints_beans:
+
+                if class_name in bean.class_name and bean.method == method:
+                    print(f"Found source. Fuzz {bean.url} with (non-exhaustive) params {chain.source_params} to reach from {class_name}.{method} to {os.path.basename(chain.stack[0])}")
+                    print(f"\t{chain}")
+                    break
+            else:
+                break
+        else:
+            if extensive:
+                logging.info(f"Code path without obvious source: {class_name}.{chain.source_functions} with params {chain.source_params}\n{chain}")
+
+if __name__ == "__main__":
+
+
+
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument('-w', '--webxml', help="/path/to/web.xml")
+    parser.add_argument('-d', '--dir', help="/path/to/source/directory (codebase to analyse)")
+    parser.add_argument('-p', '--pattern', help="Pattern to look for")
+    parser.add_argument('-f', '--file', help="Restrict the search to this file", required=False, default="")
+    parser.add_argument('-c', '--concatenation', help='Restrict the search to functions which concatenate variables with strings (easy sql injections)', required=False, default=False, action="store_true")
+    parser.add_argument("-l", "--depth", help="Max recursion level (default = 3)", default=3, type=int)
+    parser.add_argument("-m", "--max", help="Max occurrences of patterns (prevent infinite loop or state explosion)", type=int, default=5)
+    parser.add_argument("-e", "--extensive", help="Display results where the target URL (and controller) was not found in web.xml", default=False, action="store_true")
+    parser.add_argument("-r", "--results", help="limit the number of results", type=int, default=-1)
+    args = parser.parse_args()
+
+
+    pwn(args.webxml, args.dir, args.pattern, restrict_to_file=args.file, concatenation_only=args.concatenation, depth=args.depth, max_patterns=args.max, extensive=args.extensive, max_results=args.results)
